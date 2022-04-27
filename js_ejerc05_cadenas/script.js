d6dab08d85ae08a43458fd0bfcf7acd3e178d658
nahiarasanchez <96417033+nahiarasanchez@users.noreply.github.com>
2022-04-27 06:41:38 +0000

    cambios

---
+++ js_ejerc05_cadenas/script.js
@@ -0,0 +1,8 @@
+var nombre= "Nahiara";
+var apellidos= "Sánchez García";
+var primera_letra= nombre[0];
+
+document.write("<p> Las variables nombre y apellidos concatenadas resultan en: ",nombre," ",apellidos," </p>");
+
+document.write("<p> La primera letra de mi nombre es: ",primera_letra,"</p>");
+
