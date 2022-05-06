2f960208f77599494bd6fc8220b2b0b8851215c9
nahiarasanchez <96417033+nahiarasanchez@users.noreply.github.com>
2022-05-06 07:02:18 +0000

    changes

---
+++ js_ejerc07_estructura_condicional/script.js
@@ -0,0 +1,18 @@
+var testigo = window.prompt("¿Tienes el móvil?"); // si, no 
+
+  
+
+if(testigo=="si"){ 
+
+document.write("enséñame las pruebas"); 
+
+}else if(testigo=="no"){ 
+
+document.write("Te lo revisaré"); 
+
+}
+else{ 
+
+document.write("no mienta por favor"); 
+
+} 
